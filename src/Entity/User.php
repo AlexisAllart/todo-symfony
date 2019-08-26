f61e8bdf883ec0051554944b42cdccb00a7fa791
AlexisAllart <alexis.allart@gmail.com>
2019-08-26 11:12:07 +0200

    User entity init

---
+++ src/Entity/User.php
@@ -0,0 +1,300 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Colections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="users")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class User
+{
+    /**
+     * @var int
+     * 
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(type=string, length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(type=string, length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(type=string, length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(type=string, length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(type="string", length=255)
+     */
+    private $role;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(type="datetime)
+     */
+    private $updatedAt;
+
+    /**
+     * @var Task[]|ArrayCollection
+     * 
+     * @ORM\OneToMany(targetEntity="Task", mappedBy="user")
+     */
+    private $tasks;
+
+    public function __construct()
+    {
+        $this->tasks = new ArrayCollection();
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return  int
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of lastName
+     *
+     * @return  string
+     */ 
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @param  string  $lastName
+     *
+     * @return  self
+     */ 
+    public function setLastName(string $lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of firstName
+     *
+     * @return  string
+     */ 
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @param  string  $firstName
+     *
+     * @return  self
+     */ 
+    public function setFirstName(string $firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     *
+     * @return  string
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param  string  $email
+     *
+     * @return  self
+     */ 
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     *
+     * @return  string
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @param  string  $password
+     *
+     * @return  self
+     */ 
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of role
+     *
+     * @return  string
+     */ 
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set the value of role
+     *
+     * @param  string  $role
+     *
+     * @return  self
+     */ 
+    public function setRole(string $role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createdAt
+     *
+     * @return  \DateTime
+     */ 
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set the value of createdAt
+     *
+     * @param  \DateTime  $createdAt
+     *
+     * @return  self
+     */ 
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updatedAt
+     *
+     * @return  \DateTime
+     */ 
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set the value of updatedAt
+     *
+     * @param  \DateTime  $updatedAt
+     *
+     * @return  self
+     */ 
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+
+    /**
+     * Get the value of tasks
+     *
+     * @return  Task[]|ArrayCollection
+     */ 
+    public function getTasks()
+    {
+        return $this->tasks;
+    }
+
+    /**
+     * @param Task $task
+     * 
+     * @return self
+     */
+    public function addTask(Task $task) : self
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks->add($task);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Task $task
+     * 
+     * @return self
+     */
+    public function removeTask(Task $task) : self
+    {
+        $this->tasks->removeElement($task);
+
+        return $this;
+    }
+}
